fc24dd4b95240d563c32aa0b7754026f848ca4d1
Minal-singh <rishabhthakur15102@gmail.com>
2021-12-16 23:12:29 +0530

    ID changed to UUID

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,11 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 
 
 class Todo(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
